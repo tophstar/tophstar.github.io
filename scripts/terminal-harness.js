3a9734a7e49cae3bb684a412776ec975f12f72aa
tophstar <toph.roshi@gmail.com>
2016-06-18 18:24:00 -0700

    Adding elements to the page and updating look and feel

--- scripts/terminal-harness.js
+++ scripts/terminal-harness.js
@@ -1,55 +1,9 @@
 var terminalHarness = (function () {
-    /**
-     * Create the path to the player dir for the local filesystem
-     *
-     * Result should be something like: file:///Users/Ssullivan/Projects/videorx-test-harness/videorx/
-     *
-     * @return {[type]} [description]
-     */
-    var buildPathToTerminal = function () {
-            var path = '';
-            //path += window.location.pathname; // append current pathname (includes file test.html)
-            //path = path.replace(/test.html|angular.html|scaletofit.html/,''); // remove test.html from the url
-            //path += 'videorx/'; // append the rest of the path to the videorx player res dir
-
-            console.log(path);
-
-            return path;
-        },
-
-        /**
-         * Store a reference to the player element (as a jQuery object)
-         *
-         * @type {Object}
-         */
-        $terminalElement = null,
-
         /**
          * Instance of the VideoRX Player
          * @type {[type]}
          */
         terminal = null,
-
-        callTerminalMethod = function (evt) {
-            evt.preventDefault();
-
-            var methodName = $(this).html(),
-                result = eval("terminal."+methodName),
-                logMessage = "Called " + methodName + " on terminal";
-
-            if(result !== undefined) {
-                logMessage += " and received return value of: "+ result;
-            }
-
-            console.log(logMessage);
-        },
-
-        onEventReceivedFromTerminal = function (event, args) {
-            console.log("Terminal has sent an event: " + event);
-            console.log(args);
-        },
-
-        onReady = function () {},
 
         /**
          * Build the VideoRX Player
@@ -58,6 +12,7 @@ var terminalHarness = (function () {
         buildTerminal = function () {
             // Need to cache bust..
             require.config({
+                baseUrl: '',
                 paths: {
                     "terminal": "scripts/terminal.js?cachebust=" + (new Date()).getTime()
                 }
@@ -65,9 +20,7 @@ var terminalHarness = (function () {
 
             require(["terminal"], function (Terminal) {
                 var terminalOptions = {
-
                     target: "#terminal-element"
-
                 };
 
                 terminal = Terminal(terminalOptions);
@@ -82,18 +35,8 @@ var terminalHarness = (function () {
          * @return {[type]} [description]
          */
         construct: function () {
-            $terminalElement = $('#terminal-element');
-
             buildTerminal();
-
-            require.config({
-                baseUrl: buildPathToTerminal()
-            });
         },
-
-        getTerminal: function () {
-            return terminal;
-        }
     };
 })();
 
